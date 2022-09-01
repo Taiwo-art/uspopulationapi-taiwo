0a769872c9180a69858087a5d74c3d15ab09a892
Taiwo Lambo <lmb.taiwo@gmail.com>
2022-09-02 00:57:40 +0100

    Us POp Data API

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-kkkkkkk
+const getUsPopAPI = async() => {
+    let fetchPopData = await fetch("https://datausa.io/api/data?drilldowns=Nation&measures=Population")
+    let popData = await fetchPopData.json()
+    let finalPopData = popData.data
+    let finalResult = finalPopData.map(pop => `<li> ID Nation: ${pop.Nation}-----Nation: ${pop.Nation}-----ID Year: ${pop.Year}----Year: ${pop.Year}-----Population: ${pop.Population}----Slug Nation: ${pop.Nation}----</li> <br>`)
+        
+  return document.body.innerHTML = finalResult.join('');
+}
+getUsPopAPI()
